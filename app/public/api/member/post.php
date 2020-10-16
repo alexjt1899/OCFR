fef6b9cd6e724577009c148b47918a1d23df9ee2
swcindy96 <swcindy96@gmail.com>
2020-10-16 19:45:49 -0400

    edit to match database

---
+++ app/public/api/member/post.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'common.php';
+
+$db = DbConnection::getConnection();
+
+//$stmt = $db->prepare(
+//  'INSERT INTO People
+//  (EmployeeID, firstName, lastName, radioNumber, stationNumber, isActive, phone, email, address, gender, startDate, Position, CertExpiration)
+//  VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)'
+//);
+
+$stmt->execute([
+
+])
