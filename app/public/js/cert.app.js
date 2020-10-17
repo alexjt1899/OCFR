8ab3eea0fe5185b1347124081f37a22db7f739d9
alexjt1899 <ajt1@iu.edu>
2020-10-16 21:17:18 -0400

    Added some js that may or may not work

--- app/public/js/cert.app.js
+++ app/public/js/cert.app.js
@@ -1,32 +1,68 @@
-// waitingApp = new Vue({
-//   el: '#randomCert',
-//   data:{
+ waitingApp = new Vue({
+  el: '#randomCert',
+   data:{
 //     Certification: [{
 //       CertID:'',
 //       CertName:'',
-//       CerAgenc:'',
+//       CerAgency:'',
 //       CertLength:'',
 //       CertDescription:''
 //     }],
-//     newCertification:{
-//       CertID:'',
-//       CertName:'',
-//       CerAgenc:'',
-//       CertLength:'',
-//       CertDescription:''
-//     }
-//   },
-//   methods:{
-//     fetchUser(){
-//       fetch('api/member/')
-//       .then(response => response.json())
-//       .then(json => {
-//         this.users=json;
-//         console.log(this.Certification);
-//       });
-//     },
+     newCertification:{
+      CertID:'',
+      CertName:'',
+       CerAgenc:'',
+       CertLength:'',
+       CertDescription:''
+     }
+   },
+   methods:{
+     fetchUser(){
+       fetch('api/member/')
+       .then(response => response.json())
+      .then(json => {
+        this.users=json;
+         console.log(this.Certification);
+
+
+         //Alex Added stuff Here
+         CreateCertification()	{
+       			fetch('api/Certification/',{
+       				method:'POST',
+       				body: JSON.stringify(this.newCert),
+       				headers: {
+       					"Content-Type": "application/json; charset=utf-8"
+       				}
+       			})
+       			.then( response.json() )
+       			.then( json => {
+       					console.log("returned from post:", json);
+       					this.newCert.push(json[0]);
+       					this.newCert = this.newCertData();
+       			});
+       			console.log("creating (POSTing)")
+       			console.log(this.newCert);
+
+       	},
+
+       	newCertData() {
+       			return{
+       				CertID: "",
+       				CertName: "",
+       				CerAgency: "",
+       				CertLength:"",
+       				CertDescription:""
+       			}
+       	}
+
+
+       	}
+      
+
+       });
+     },
 //     createUser(){
-//       this.newCertification.CertID = (this.newCertification.)
+//      this.newCertification.CertID = (this.newCertification.)
 //     }
 //   }
 //
