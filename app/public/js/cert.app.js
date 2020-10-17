03c56c31f6ec60f5e76e1782a1d40386db466c56
swcindy96 <swcindy96@gmail.com>
2020-10-16 21:11:25 -0400

    cindy struggling with js

---
+++ app/public/js/cert.app.js
@@ -0,0 +1,33 @@
+// waitingApp = new Vue({
+//   el: '#randomCert',
+//   data:{
+//     Certification: [{
+//       CertID:'',
+//       CertName:'',
+//       CerAgenc:'',
+//       CertLength:'',
+//       CertDescription:''
+//     }],
+//     newCertification:{
+//       CertID:'',
+//       CertName:'',
+//       CerAgenc:'',
+//       CertLength:'',
+//       CertDescription:''
+//     }
+//   },
+//   methods:{
+//     fetchUser(){
+//       fetch('api/member/')
+//       .then(response => response.json())
+//       .then(json => {
+//         this.users=json;
+//         console.log(this.Certification);
+//       });
+//     },
+//     createUser(){
+//       this.newCertification.CertID = (this.newCertification.)
+//     }
+//   }
+//
+// })
