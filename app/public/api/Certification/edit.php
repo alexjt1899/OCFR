082b7f34930ecbd3b3affefe25e449147f6d5034
tangyuw <tangyuw@iu.edu>
2020-11-01 19:26:02 -0500

    what happened.... the correct edit part

--- app/public/api/Certification/edit.php
+++ app/public/api/Certification/edit.php
@@ -8,7 +8,6 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-<<<<<<< HEAD
   'UPDATE Certification SET CertName=?,CerAgency = ?,CertLength = ?,CertDescription = ?
    WHERE CertID = ?;'
 );
@@ -30,27 +29,3 @@ $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.ph
 header('HTTP/1.1 303 See Other');
 header('Content-Type: application/json');
 header('Location: ../Members/?Member_ID=' . $pk);
-=======
-  "UPDATE TABLE Certification
-  SET CertName='Test'
-  WHERE CertID=29"
-);
-
-$stmt->execute([
-  $_POST['CertID'],
-  $_POST['CertName']
-  ]);
-/*  $_POST['CerAgency'],
-  $_POST['CertLength'],
-  $_POST['CertDescription'] */
-
-
-$CertId = $db->lastInsertID();
-header('HTTP/1.1 303 See Other');
-
-header('Location: ../Certification/');
-?>
-
--- If auto generate works, remove CertID from prepare, execute
---$CertID = $db->lastInsertID(); -->
->>>>>>> d2dd8ebf5a888c0256a99dc352f5a91a92b19a6f
